bf978b57cbcf77e1ed41a99078ddabfc1cfe1636
vu nguyen <vutrinhsat2023@gmail.com>
2023-07-17 17:21:58 +0700

    tong_quan

---
+++ M2/hello/tong_quan.php/form-post.php
@@ -0,0 +1,37 @@
+<?php
+    // $_SERVER: thông tin máy chủ
+    // $_POST  : nhận dữ liệu gửi lên qua method POST
+    // $_GET   : nhận dữ liệu gửi lên qua method GET
+    // $_REQUEST : nhận dữ liệu gửi lên qua method GET
+
+    // Kiểm tra người dùng đã gửi dữ liệu lên hay chưa
+    
+    if( $_SERVER['REQUEST_METHOD'] == 'POST' ){
+        // Xử lý
+        // Lấy giá trị gửi lên
+        echo '<pre>';
+        print_r($_REQUEST);
+        echo '</pre>';
+
+        // Lưu trữ
+        $ho_va_ten = $_REQUEST['ho_va_ten'];
+        $mat_khau = $_REQUEST['mat_khau'];
+
+        // Xử lý
+        if( $ho_va_ten == 'admin' && $mat_khau == '123456' ){
+            echo 'Chao mung admin';
+        }else{
+            echo 'Tai khoan khong dung';
+        }
+        
+    }
+    // echo '<pre>';
+    // print_r($_SERVER);
+    // echo '</pre>';
+
+?>
+<form action="" method="post">
+    Ten dang nhap: <input type="text" name="ho_va_ten" > <br>
+    Mat khau: <input type="password" name="mat_khau" > <br>
+    <input type="submit" value="Dang nhap">
+</form>
--- M2/hello/vu.php
+++ M2/hello/vu.php
@@ -1,2 +1,2 @@
 <?php
-echo 'đâsdsadas';
+echo 'Vũ';
