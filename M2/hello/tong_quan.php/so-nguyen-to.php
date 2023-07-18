05be6ea944e6b61411d601c31bfd5745bc9f99ec
vu nguyen <vutrinhsat2023@gmail.com>
2023-07-18 14:14:56 +0700

    so-nguyen-to

---
+++ M2/hello/tong_quan.php/so-nguyen-to.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    function isPrime($number) {
+        if ($number < 2) {
+            return false;
+        }
+    
+        for ($i = 2; $i <= sqrt($number); $i++) {
+            if ($number % $i === 0) {
+                return false;
+            }
+        }
+    
+        return true;
+    }
+    
+    for ($i = 2; $i < 100; $i++) {
+        if (isPrime($i)) {
+            echo $i ." ";
+            echo "<br>";
+        }
+    }
+    ?>
+</body>
+</html>
