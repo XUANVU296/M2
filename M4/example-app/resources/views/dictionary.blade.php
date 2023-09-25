93584b806bf4e6f77867fae11f7e5850c684e418
vu nguyen <vutrinhsat2023@gmail.com>
2023-09-25 11:53:53 +0700

    dictionanry

---
+++ M4/example-app/resources/views/dictionary.blade.php
@@ -0,0 +1,6 @@
+<form action="/dictionary" method="post">
+    @csrf
+    <h2>DICTIONARY ENGLISH-VIETNAM</h2>
+    Nhập từ cần dịch: <input type="text" name="english" id="">
+    <input type="submit" value="Dịch">
+</form>
